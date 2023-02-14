8f29d68a4f9af78559af057e428897ba71156290
Caio Rocha <crc@cin.ufpe.br>
2023-02-13 21:28:59 -0300

    .

--- kernel.asm
+++ kernel.asm
@@ -3,14 +3,38 @@ jmp 0x0000:start
 
 data:
 	
-	;Dados do projeto...
+    title db 'JOGO'
+	op_menu db 1
+
+clear_screen:
+    mov ah, 0bh
+    mov bh, 00h
+    mov bl, 6
+    int 10h
+    ret
 
 start:
-    xor ax, ax
+    xor ax, ax  ;zera registradores
     mov ds, ax
     mov es, ax
+    mov cx, ax
+
+    mov ah, 0
+    mov al, 13h
+    int 10h  ;inicia modo de video
+
+    call clear_screen
     
-    ;Código do projeto...
+    mov si, op_menu
+    lodsb   ;carrega valor de op_menu em al
+    dec si
+    
+    ;verifica a opcao selecionada
+    cmp al, 1
+
+    cmp al, 2
+
+    cmp al, 3
 
    
 
